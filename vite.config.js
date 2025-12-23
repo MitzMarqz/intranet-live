6157fdb3bb0df7ea045d139c350fff10ddd610c6
mg_e <89415673+mg-e@users.noreply.github.com>
2025-12-23 21:15:29 +0800

    prep for gh pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: 'intranet-live-myrepo',  // <-- Replace <repo-name> with your GitHub repository name
   plugins: [react()],
   server: {
     proxy: {
