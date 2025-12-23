da2f242ad4b5560d762a8c04992d954ae06ed61c
mg_e <89415673+mg-e@users.noreply.github.com>
2025-12-23 21:46:41 +0800

    Prepare app for GitHub Pages deployment under MitzMarqz

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: 'intranet-live-myrepo',  // <-- Replace <repo-name> with your GitHub repository name
+  base: 'intranet-live',  // <-- Replace <repo-name> with your GitHub repository name
   plugins: [react()],
   server: {
     proxy: {
